1fe018c9763755e0f5ffbe034761a41dab2e497d
Builder <builder@nvidia-vgpu-testing-infra>
2026-09-21 19:20:44 -0300

    feat(terraform): add 12 libvirt domains for control-plane VMs

---
+++ terraform/domains.tf
@@ -0,0 +1,41 @@
+resource "libvirt_domain" "vgpu_vm" {
+  for_each = local.vm_definitions
+
+  name        = each.value.name
+  type        = "kvm"
+  memory      = each.value.memory
+  memory_unit = "MiB"
+  vcpu        = each.value.vcpus
+  running     = true
+
+  os = {
+    type         = "hvm"
+    type_arch    = "x86_64"
+    type_machine = "q35"
+    boot_devices = [{ dev = "hd" }]
+  }
+
+  cpu = {
+    mode = "host-passthrough"
+  }
+
+  devices = {
+    disks = [
+      {
+        source = { volume = { pool = libvirt_pool.vgpu.name, volume = libvirt_volume.vm_root[each.key].name } }
+        target = { dev = "vda", bus = "virtio" }
+      },
+      {
+        source = { file = { file = libvirt_volume.cloudinit[each.key].path } }
+        target = { dev = "sda", bus = "sata" }
+      }
+    ]
+    interfaces = [
+      {
+        model       = { type = "virtio" }
+        source      = { network = { network = libvirt_network.mgmt.name } }
+        wait_for_ip = { source = "lease", timeout = 300 }
+      }
+    ]
+  }
+}
